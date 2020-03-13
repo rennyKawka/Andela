e821ad2addcf7fc3bdf8341b1c5795ea2c61cc7b
rennyKawka <52996600+rennyKawka@users.noreply.github.com>
2020-03-13 17:13:42 +0300

    just a summery
    Summery wrap down

---
+++ practices.py
@@ -0,0 +1,58 @@
+# 01 *** My practices ***
+
+# line 19 and 20 I wanted to construt a string for the results
+
+# 02 functions
+"""
+def say_hi(name):
+    print("hello " + name + " " + "welcome To my page")
+say_hi(input("enter your name: "))
+"""
+
+# 03 '''Finding the squares'''
+"""
+def square(num):
+    return num*num
+
+results = square(int(input('Enter a value, we calculate its Square: ')))
+
+# errror
+
+# num = square(num)
+# print("The square of " + num + " " + results)
+
+
+print(results)
+"""
+
+# 04 If '''finding the maximum value'''
+"""
+# case1 for maximum value;
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+print(max_num(394, 399, 396))
+"""
+
+# 05 '''A simpple calculator'''
+
+num1 = int(input("Enter First number: "))
+op = input("Enter operator: ")
+num2 = int(input("Enter Second number: "))
+
+if op == '+':
+    print(num1 + num2)
+elif op == '-':
+    print(num1 - num2)
+elif op == '*':
+    print(num1 * num2)
+elif op == '/':
+    print(num1 / num2)
+else:
+    print("Invalid operator!!!")
+
