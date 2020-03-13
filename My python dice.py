7834509865d2cdda529bb4e4470559f9808850df
rennyKawka <52996600+rennyKawka@users.noreply.github.com>
2020-03-13 05:04:23 -0700

    my simpleDie.py

--- My python dice.py
+++ My python dice.py
@@ -1,13 +1,8 @@
 import random
 min = 1
 max = 6
-
 roll_again = "yes"
-
 while roll_again == "yes" or roll_again == "y":
-    print "Rolling the dices..."
-    print "The values are...."
-    print random.randint(min, max)
-    print random.randint(min, max)
-
-    roll_again = raw_input("Roll the dices again?")
+    print ("Rolling the dices...")
+    print (random.randint(min, max))
+    roll_again = input("Roll the dices again? ")
