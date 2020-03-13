8cf6ae3115d06a8cc109a7e2fbf5c167f01113dc
rennyKawka <52996600+rennyKawka@users.noreply.github.com>
2020-03-13 18:15:16 +0300

    Update My python dice.py
    just cool a Dice Game

--- My python dice.py
+++ My python dice.py
@@ -3,6 +3,13 @@ min = 1
 max = 6
 roll_again = "yes"
 while roll_again == "yes" or roll_again == "y":
-    print ("Rolling the dices...")
-    print (random.randint(min, max))
-    roll_again = input("Roll the dices again? ")
+
+  print ("Rolling the dices...")
+  number = (random.randint(min, max))
+  print (number)  
+    # if random(min, max) == 1:
+    #   print ("Owh Sorry!!")
+
+  if number == 6:
+    print("wow you WIN \n GOOD LUCK")
+  roll_again = input("Roll the dices again? ")
