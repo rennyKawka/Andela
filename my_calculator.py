87ccd60eeb3b1e284a723dac2bd0fe19a4911493
rennyKawka <52996600+rennyKawka@users.noreply.github.com>
2020-03-16 16:57:02 +0300

    Update my_calculator.py
    A calcuclator for BODMAS

--- my_calculator.py
+++ my_calculator.py
@@ -1,15 +1,15 @@
-import random
-min = 1
-max = 6
-roll_again = "yes"
-while roll_again == "yes" or roll_again == "y":
 
-  print ("Rolling the dices...")
-  number = (random.randint(min, max))
-  print (number)  
-    # if random(min, max) == 1:
-    #   print ("Owh Sorry!!")
+num1 = int(input("Enter First number: "))
+op = input("Enter operator: ")
+num2 = int(input("Enter Second number: "))
 
-  if number == 6:
-    print("wow you WIN \n GOOD LUCK")
-  roll_again = input("Roll the dices again? ")
+if op == '+':
+    print(num1 + num2)
+elif op == '-':
+    print(num1 - num2)
+elif op == '*':
+    print(num1 * num2)
+elif op == '/':
+    print(num1 / num2)
+else:
+    print("Invalid operator!!!")
